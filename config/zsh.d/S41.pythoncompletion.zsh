cbdb8606ce2b9601bc478eddd94d0476f333f881
Keith Gaughan <k@stereochro.me>
2013-07-27 18:10:05 +0100

    Replace ~ with $HOME

--- config/zsh.d/S41.pythoncompletion.zsh
+++ config/zsh.d/S41.pythoncompletion.zsh
@@ -1,4 +1,4 @@
 # See: https://github.com/jbisbee/python-shell-enhancement
 
-export PYTHONSTARTUP="~/.local/share/python/pythonstartup.py"
+export PYTHONSTARTUP="$HOME/.local/share/python/pythonstartup.py"
 export PYTHON_HISTORY_FILE="${XDG_CACHE_HOME-$HOME/.cache}/python_history"
