d6c25752231de8e63678d16805ee2098e2d3e6e7
Keith Gaughan <k@stereochro.me>
2013-08-05 02:57:04 +0100

    If the python startup file hasn't been symlinked, ignore it.

--- config/zsh.d/S41.pythoncompletion.zsh
+++ config/zsh.d/S41.pythoncompletion.zsh
@@ -1,4 +1,6 @@
 # See: https://github.com/jbisbee/python-shell-enhancement
 
-export PYTHONSTARTUP="$HOME/.local/share/python/pythonstartup.py"
-export PYTHON_HISTORY_FILE="${XDG_CACHE_HOME-$HOME/.cache}/python_history"
+if test -e "$HOME/.local/share/python/pythonstartup.py"; then
+    export PYTHONSTARTUP="$HOME/.local/share/python/pythonstartup.py"
+    export PYTHON_HISTORY_FILE="${XDG_CACHE_HOME-$HOME/.cache}/python_history"
+fi
