74afbb436a5c5b86d2379869b5966828e76aeec5
Keith Gaughan <k@stereochro.me>
2014-05-02 19:59:02 +0100

    Add the vim-go plugin and set up environment for it.

--- config/zsh.d/S20.environment.zsh
+++ config/zsh.d/S20.environment.zsh
@@ -1,4 +1,10 @@
 mkdir -p $HOME/.local/{lib,share} $HOME/{.cache,.config}
 
+# Go.
+
+export GOPATH=$HOME/Projects/go
+mkdir -p $GOPATH
+export PATH=$PATH:$GOPATH/bin
+
 # file permissions: rwxr-xr-x
 umask 022
