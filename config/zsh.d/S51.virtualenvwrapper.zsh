2aa4ee9967efb9a30e51a6a3ef0829ee7e4104c1
Keith Gaughan <k@stereochro.me>
2014-07-05 03:51:07 +0100

    Slim down the virtualenvwrapper loader.

--- config/zsh.d/S51.virtualenvwrapper.zsh
+++ config/zsh.d/S51.virtualenvwrapper.zsh
@@ -1,50 +1,17 @@
-# Dynamically load virtualenvwrapper functions to reduce shell startup
-# time.
-#
-# Copyright 2012 Aron Griffis <aron@arongriffis.com>
-# Released under the GNU GPL v3
-#######################################################################
-#
-# Taken from:
-# https://gist.github.com/2483204
-
 export WORKON_HOME=$HOME/.virtualenvs
 
-# Python virtualenvwrapper loads really slowly, so load it on demand.
-if [[ $(type -w workon) != "workon: function" ]]; then
-  virtualenv_funcs=( workon deactivate mkvirtualenv )
+_source_first () {
+	file=$1
+	shift
+	while test $# -gt 0; do
+		if test -e $1/$file; then
+			source $1/$file
+			break
+		fi
+		shift
+	done
+}
 
-  load_virtualenv() {
-    # If these already exist, then virtualenvwrapper won't override them.
-    unset -f "${virtualenv_funcs[@]}"
+_source_first virtualenvwrapper_lazy.sh /usr/local/bin /usr/share/virtualenvwrapper
 
-    # virtualenvwrapper doesn't load if PYTHONPATH is set, because the
-    # virtualenv python doesn't have the right modules.
-    declare _pp="$PYTHONPATH"
-    unset PYTHONPATH
-
-    # Attempt to load virtualenvwrapper from its many possible sources...
-    _try_source() { [[ -f $1 ]] || return; source "$1"; return 0; }
-    _try_source /usr/local/bin/virtualenvwrapper.sh || \
-    _try_source /etc/bash_completion.d/virtualenvwrapper || \
-    _try_source /usr/bin/virtualenvwrapper.sh
-    return_status=$?
-    unset -f _try_source
-
-    # Restore PYTHONPATH
-    [[ -n $_pp ]] && export PYTHONPATH="$_pp"
-
-    # Did loading work?
-    if [[ $return_status != 0 || $(type -w "$1") != "$1: function" ]]; then
-      echo "Error loading virtualenvwrapper, sorry" >&2
-      return $return_status
-    fi
-
-    # Chain-load the appropriate function
-    "$@"
-  }
-
-  for v in "${virtualenv_funcs[@]}"; do
-    eval "$v() { load_virtualenv $v \"\$@\"; }"
-  done
-fi
+unset -f _source_first
