c2c62d34859391ae2f444667ad4ba3a6b7ef709e
Keith Gaughan <keith@blacknight.com>
2014-08-08 16:43:15 +0100

    Fuck you, Debian. Just... fuck you.

--- config/zsh.d/S51.virtualenvwrapper.zsh
+++ config/zsh.d/S51.virtualenvwrapper.zsh
@@ -1,17 +1,30 @@
 export WORKON_HOME=$HOME/.virtualenvs
 
-_source_first () {
-	file=$1
-	shift
-	while test $# -gt 0; do
-		if test -e $1/$file; then
-			source $1/$file
-			break
-		fi
-		shift
+export VIRTUALENVWRAPPER_SCRIPT="$(which virtualenvwrapper.sh 2>/dev/null >&2)"
+if [ -z "$VIRTUALENVWRAPPER_SCRIPT" ]; then
+	if [ -e /etc/bash_completion.d/virtualenvwrapper ]; then
+		export VIRTUALENVWRAPPER_SCRIPT=/etc/bash_completion.d/virtualenvwrapper
+	else
+		echo "ERROR: virtualenvwrapper_quick.sh: Could not find virtualenvwrapper.sh" >&2
+	fi
+fi
+
+# Load the real implementation of the API from virtualenvwrapper.sh
+function virtualenvwrapper_load {
+	source "$VIRTUALENVWRAPPER_SCRIPT"
+}
+
+# Set up "alias" functions based on the API definition.
+function virtualenvwrapper_setup_lazy_loader {
+	typeset venvw_name
+	for venvw_name in mkvirtualenv rmvirtualenv lsvirtualenv showvirtualenv workon add2virtualenv cdsitepackages cdvirtualenv lssitepackages toggleglobalsitepackages cpvirtualenv setvirtualenvproject mkproject cdproject mktmpenv; do
+		eval "
+function $venvw_name {
+	virtualenvwrapper_load
+	$venvw_name \"\$@\"
+}
+"
 	done
 }
 
-_source_first virtualenvwrapper_lazy.sh /usr/local/bin /usr/share/virtualenvwrapper /usr/share/doc/virtualenvwrapper/examples
-
-unset -f _source_first
+virtualenvwrapper_setup_lazy_loader
