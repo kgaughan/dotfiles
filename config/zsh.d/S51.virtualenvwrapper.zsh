53b61af6b79f96890333dd2793ad9db5cd80f6e6
Keith Gaughan <k@stereochro.me>
2014-08-31 19:39:36 +0100

    This is getting more and more annoying...

--- config/zsh.d/S51.virtualenvwrapper.zsh
+++ config/zsh.d/S51.virtualenvwrapper.zsh
@@ -1,38 +1,38 @@
 export WORKON_HOME=$HOME/.virtualenvs
 
 # Try the relatively sane options first.
-for i in virtualenvwrapper virtualenvwrapper.sh; do
-	if which $i 2>/dev/null >&2; then
-		export VIRTUALENVWRAPPER_SCRIPT="$(which $i)"
+while read i; do
+	if test ! -z "$i" -a -e "$i"; then
+		export VIRTUALENVWRAPPER_SCRIPT="$i"
 		break
 	fi
-done
+done <<FIN
+/usr/share/virtualenvwrapper/virtualenvwrapper.sh
+$(which virtualenvwrapper 2>/dev/null >&2)
+$(which virtualenvwrapper.sh 2>/dev/null >&2)
+/etc/bash_completion.d/virtualenvwrapper
+FIN
 
 if [ -z "$VIRTUALENVWRAPPER_SCRIPT" ]; then
-	# OK, let's give Debian a stab now.
-	if [ -e /etc/bash_completion.d/virtualenvwrapper ]; then
-		export VIRTUALENVWRAPPER_SCRIPT=/etc/bash_completion.d/virtualenvwrapper
-	else
-		echo "ERROR: Could not find virtualenvwrapper" >&2
-	fi
-fi
+	echo "ERROR: Could not find virtualenvwrapper" >&2
+else
+	# Load the real implementation of the API from virtualenvwrapper.sh
+	function virtualenvwrapper_load {
+		source "$VIRTUALENVWRAPPER_SCRIPT"
+	}
 
-# Load the real implementation of the API from virtualenvwrapper.sh
-function virtualenvwrapper_load {
-	source "$VIRTUALENVWRAPPER_SCRIPT"
-}
-
-# Set up "alias" functions based on the API definition.
-function virtualenvwrapper_setup_lazy_loader {
-	typeset venvw_name
-	for venvw_name in mkvirtualenv rmvirtualenv lsvirtualenv showvirtualenv workon add2virtualenv cdsitepackages cdvirtualenv lssitepackages toggleglobalsitepackages cpvirtualenv setvirtualenvproject mkproject cdproject mktmpenv; do
-		eval "
+	# Set up "alias" functions based on the API definition.
+	function virtualenvwrapper_setup_lazy_loader {
+		typeset venvw_name
+		for venvw_name in mkvirtualenv rmvirtualenv lsvirtualenv showvirtualenv workon add2virtualenv cdsitepackages cdvirtualenv lssitepackages toggleglobalsitepackages cpvirtualenv setvirtualenvproject mkproject cdproject mktmpenv; do
+			eval "
 function $venvw_name {
 	virtualenvwrapper_load
 	$venvw_name \"\$@\"
 }
 "
-	done
-}
+		done
+	}
 
-virtualenvwrapper_setup_lazy_loader
+	virtualenvwrapper_setup_lazy_loader
+fi
