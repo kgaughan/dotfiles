35eb4b7427a3ff4510b9df53abd9b02fd19a3e53
Keith Gaughan <k@stereochro.me>
2016-04-01 11:29:30 +0000

    Home directory position independence.

--- config/zsh.d/S40.completion.zsh
+++ config/zsh.d/S40.completion.zsh
@@ -1,3 +1,3 @@
 zstyle ':completion:*' completer _expand _complete _approximate
 zstyle ':completion:*' max-errors 0 not-numeric
-zstyle :compinstall filename '/home/keith/.zshrc'
+zstyle :compinstall filename "$HOME/.zshrc"
