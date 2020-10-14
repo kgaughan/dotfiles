639da47c79f9e7d6a458f80b0bbeef3ba1a2f1b3
Keith Gaughan <keith@gaughan.ie>
2020-10-14 15:26:56 +0100

    Function to show Go dependencies with updates

---
+++ common/.config/fish/functions/go-list-mod-updates.fish
@@ -0,0 +1,3 @@
+function go-list-mod-updates -d "List available Go dependency updates"
+	go list -u -f '{{if (and (not (or .Main .Indirect)) .Update)}}{{.Path}}: {{.Version}} -> {{.Update.Version}}{{end}}' -m all 2> /dev/null
+end
