0bc4662bb04bc4a287550d0a66b45e5f30531e9a
Sourcery AI <>
2022-11-18 14:39:48 +0000

    'Refactored by Sourcery'

--- common/.ipython/ipy_user_conf.py
+++ common/.ipython/ipy_user_conf.py
@@ -105,7 +105,7 @@ def main():
 def import_all(modules):
     """ Usage: import_all("os sys") """
     for m in modules.split():
-        ip.ex("from %s import *" % m)
+        ip.ex(f"from {m} import *")
 
 def execf(fname):
     """ Execute a file in user namespace """
